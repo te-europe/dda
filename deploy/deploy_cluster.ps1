6c041c52069ac017d27a23a3619407be9ab7ddba
Jack Minchin <jack@thisisstratum.co.uk>
2022-05-21 19:46:47 +0100

    Completed deploy and remove script

--- deploy/deploy_cluster.ps1
+++ deploy/deploy_cluster.ps1
@@ -16,15 +16,44 @@ function Deploy {
         [int] $MemoryInGB
     )
 
-    Write-Progress -Activity "Deploying a Data Analytics cluster to the resource group '$ResourceGroupName'..."
+    # Write-Progress -Activity "Deploying a Data Analytics cluster to the resource group '$ResourceGroupName'..."
 
-    Invoke-Expression "az deployment group create --resource-group $ResourceGroupName --template-file ./utils/az-dask-scheduler-deploy.bicep"
+    # Download the scheulder BICEP template
+    $SchedulerTemplate = "https://raw.githubusercontent.com/te-europe/dda/master/resources/az-dask-scheduler-deploy.bicep"
+    $WorkerTemplate = "https://raw.githubusercontent.com/te-europe/dda/master/resources/az-dask-worker-deploy.bicep"
+    Invoke-WebRequest -Uri $SchedulerTemplate -OutFile "./az-dask-scheduler-deploy.bicep"
+    Invoke-WebRequest -Uri $WorkerTemplate -OutFile "./az-dask-worker-deploy.bicep"
 
-    Write-Progress -Completed "Deployment completed."
+    # Create the resources in Azure
+    Invoke-Expression "az deployment group create --resource-group $ResourceGroupName --template-file ./az-dask-scheduler-deploy.bicep"
+    Invoke-Expression "az deployment group create --resource-group $ResourceGroupName --template-file ./az-dask-worker-deploy.bicep --parameters NumberOfCores=$NumberOfCores MemoryInGB=$MemoryInGB"
+
+    # Delete the BICEP template
+    Remove-Item -Path "./az-dask-worker-deploy.bicep" -Force
+    Remove-Item -Path "./az-dask-scheduler-deploy.bicep" -Force
+
+}
+
+function Remove {
+    # Remove a Data Analytics cluster from the specified resource group.
+
+    # The parameter for the resource group
+    param(
+        [Parameter(Position = 0, Mandatory = $true, HelpMessage = "The resource group name.")]
+        [string] $ResourceGroupName
+    )
+
+
+    # Delete the resources in Azure
+    Invoke-Expression "az container delete --resource-group $ResourceGroupName --name daskscheduler -y"
+    Invoke-Expression "az container delete --resource-group $ResourceGroupName --name daskworker -y"
+
+
 }
 
 
 switch ($Command) {
     "deploy" { Deploy }
+    "remove" { Remove }
     Default {}
 }
